4d3f51d97245d5e2335725e9032aef948c683d67
Santiago <santiagorodriguezmartinez00@gmail.com>
2026-07-17 18:45:53 -0600

    Trigésimoquinto commit: Pregunta 1 ACT5

---
+++ Actividad_Listas/P1-Contar_pares_e_impares.py
@@ -0,0 +1,19 @@
+#Contar pares e impares
+def contar_pares_impares(numeros):
+    pares = 0
+    impares = 0
+    for num in numeros:
+        if num % 2 == 0:
+            pares += 1
+        else:
+            impares += 1
+    return pares, impares
+
+numeros = []
+for i in range(10):  # pide 10 numeros y los va guardando
+    num = int(input("Número {}: ".format(i+1)))
+    numeros.append(num)
+
+p, i = contar_pares_impares(numeros)
+print("Pares:", p)
+print("Impares:", i)
